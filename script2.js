0757b9f5be910be6f49ebea77bc848d6b536369a
josueeeee25 <bodot30@gmail.com>
2023-09-05 11:34:29 +0800

    assignment

---
+++ script2.js
@@ -0,0 +1,7 @@
+function ianlang(create) {
+    var josue = 150;
+    var ben = create.length;
+    var dove = josue - ben
+    
+    document.getElementById("ben").textContent=`${ben}chararters (${dove}remaining)`;
+}
